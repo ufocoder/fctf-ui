a08225199f0dafb1628230140dfa1a4caef2d7e4
Sergey Ufocoder <xufocoder@gmail.com>
2019-11-09 01:34:45 +0300

    Fix env variable

--- src/services/PlanetService/index.js
+++ src/services/PlanetService/index.js
@@ -1,6 +1,6 @@
 import defaultPlanets from './data'
 
-const API_URL = process.env.API_URL || 'http://127.0.0.1:5000'
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000'
 
 const withPostParams = (bodyParams) => ({
     method: 'POST',
