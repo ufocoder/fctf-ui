11ccf4a1d4401b476ab4e4e8a57124bf00c4d99d
Sergey Ufocoder <xufocoder@gmail.com>
2019-11-09 18:40:58 +0300

    Fix `Popup`

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -11,20 +11,20 @@ const Popup = ({ onCloseClick }) => {
             <h2 className="popup__title">Hello, colonizer!</h2>
           </div>
           <div className="popup__body">
-            <p className = "popup__text"> 
+            <p className="popup__text"> 
               It's the FCTF #3<br/>CTF for frontend developers <br />So we have hidden 8 flags
             </p>
-            <p className = "popup__text"> 
+            <p className="popup__text"> 
               The flag in one of tasks will be consist from several words You must replace spaces between with underscores. 
             </p>
-            <p className = "popup__text"> 
-              For example, here is a key: 
-              <pre>lorem ipsum dolor sit amet</pre>
+            <p className="popup__text"> 
+              For example, here is a key:
             </p>
-            <p className = "popup__text"> 
+            <pre className="popup__code">lorem ipsum dolor sit amet</pre>
+            <p className="popup__text"> 
               You must use flag above like the following:
-              <pre>lorem_ipsum_dolor_sit_amet</pre>
             </p>
+            <pre className="popup__code">lorem_ipsum_dolor_sit_amet</pre>
             <div className="popup__close" onClick={onCloseClick}>
               Play!
             </div>
