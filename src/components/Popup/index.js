c5e19b8559c7db8d677309d671f0849f02639d38
Sergey Ufocoder <xufocoder@gmail.com>
2019-11-09 03:35:36 +0300

    Update `Popup`. Add help text

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -2,20 +2,32 @@ import React from 'react';
 
 import './style.css';
 
-
-const Popup = ({onCloseClick}) => {
+const Popup = ({ onCloseClick }) => {
   return (
     <div className="layout__popup">
       <div className="popup__overlay">
         <div className="popup__content">
           <div className="popup__header">
-            <h2 className="popup__title">Hello, colonizer</h2>
+            <h2 className="popup__title">Hello, colonizer!</h2>
           </div>
           <div className="popup__body">
-            <p className = "popup__text"> Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book...</p>
-            <div className="popup__close" 
-                  onClick={onCloseClick}>
-              close
+            <p className = "popup__text"> 
+              There's the FCTF #3 <br/>CTF for frontend developers <br />So we hide 8 flags
+            </p>
+            <p className = "popup__text"> 
+              The flag in one of tasks will be constist from several words You must replace spaces beetween with underscore. 
+            </p>
+            
+            <p className = "popup__text"> 
+              For example, there's a key: 
+              <pre>lorem ipsum dolor sit amet</pre>
+            </p>
+            <p className = "popup__text"> 
+              You must use flag above like the following:
+              <pre>lorem_ipsum_dolor_sit_amet</pre>
+            </p>
+            <div className="popup__close" onClick={onCloseClick}>
+              Play!
             </div>
           </div>
         </div>
