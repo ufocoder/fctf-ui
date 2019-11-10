4855261c56fc025b1d3296d4b2a48dfd0ef1b22f
Sergey Ufocoder <xufocoder@gmail.com>
2019-11-10 11:47:48 +0300

    Fix typo

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -15,7 +15,7 @@ const Popup = ({ onCloseClick }) => {
               It's the FCTF #3<br/>CTF for frontend developers <br />So we have hidden 8 flags
             </p>
             <p className="popup__text"> 
-              The flag in one of tasks will be consist from several words You must replace spaces between with underscores. 
+              The flag in one of tasks will be consisted from several words You must replace spaces between with underscores. 
             </p>
             <p className="popup__text"> 
               For example, here is a key:
